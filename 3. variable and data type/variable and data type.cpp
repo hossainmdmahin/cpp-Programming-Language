2c4505d8d44f587aa399001363fb95f84f816a1d
Mahin <190971278+hossainmdmahin@users.noreply.github.com>
2026-04-01 11:37:05 +0600

    Add files via upload

---
+++ 3. variable and data type/variable and data type.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+
+
+    int mynum = 5;                  // Integer (whole number without decimals)
+    double myFloatNum = 5.99;       // Floating point number (with decimals)
+    char myLetter = 'D';            // Character
+    string myText = "Hello";        // String (text)
+    bool myBoolean = true;
+
+
+    cout<<" my num : "<< mynum <<endl;
+    cout<<" myFloatNum : "<< myFloatNum <<endl;
+    cout<<" myLetter : "<< myLetter <<endl;
+    cout<<" myText : "<< myText <<endl;
+    cout<<" myBoolean : "<<myBoolean <<endl;
+    cout<< endl;
+
+
+
+   return 0;
+
+
+
+}
