2a6a5ba8ef351021e836e93a12860802953196e2
Mahin <190971278+hossainmdmahin@users.noreply.github.com>
2026-04-01 11:43:14 +0600

    Add files via upload

---
+++ 3. variable and data type/constant.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  const int myNum = 15;
+
+  cout << " constant output is : "<< myNum;
+  return 0;
+}
+
+/*
+output:
+
+constant output is : 15
+
+Process returned 0 (0x0)   execution time : 0.166 s
+Press any key to continue.
+
+*/
