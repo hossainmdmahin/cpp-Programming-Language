d7a4ad678ffe53cc4be36f3b2247a613096d0929
Mahin <190971278+hossainmdmahin@users.noreply.github.com>
2026-04-01 12:01:13 +0600

    Create ommiting namespace.cpp

---
+++ 7. string/ommiting namespace.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+// using namespace std; - Remove this line
+
+int main() {
+  std::string greeting = "Hello"; // output - hello
+  std::cout << greeting;
+  return 0;
+}
