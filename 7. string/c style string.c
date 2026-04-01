9ddeda6b233513f908e5c70477c1a3441e916e98
Mahin <190971278+hossainmdmahin@users.noreply.github.com>
2026-04-01 12:02:57 +0600

    Add C-style string example in C++

---
+++ 7. string/c style string.c
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string greeting1 = "Hello";  // Regular String
+  char greeting2[] = "Hello";  // C-Style String (an array of characters)
+  cout << greeting1 << "\n";
+  cout << greeting2;
+  return 0;
+}
