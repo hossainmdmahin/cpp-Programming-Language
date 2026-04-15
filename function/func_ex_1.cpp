5d2594555e1e4eb5deb5e960ba346ac345da4f6e
Mahin <190971278+hossainmdmahin@users.noreply.github.com>
2026-04-16 01:05:22 +0600

    Refactor add_number function and add rectangle function

--- function/func_ex_1.cpp
+++ function/func_ex_1.cpp
@@ -3,10 +3,19 @@
 using namespace std;
 
 void add_number(int a, int b){
-    int result;
-    result = a + b;
-    cout << "The sum is: " << result << endl;
+    int result1;
+    result1 = a + b;
+    cout << "The sum is: " << result1 << endl;
 }
+
+// function definition
+int rectangle(int x, int y) {
+
+    return x * y;
+    cout << "The rectangle area is: " << x * y << endl;
+
+}
+
 
 int main(){
     int x, y;
@@ -17,7 +26,11 @@ int main(){
     cout << "Enter your second number: ";
     cin >> y;
 
+
     add_number(x, y);
+
+    int area = rectangle(x, y);
+    cout << "The rectangle area is: " << area << endl;
 
     getch();
 }
